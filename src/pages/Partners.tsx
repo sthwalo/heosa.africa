358b17c6930f2cdb39dd5bcb022f1b93cec9ebb3
Immaculate Nyoni <sthwaloe@gmail.com>
2025-01-06 16:37:51 +0200

    update partners and sponsors

--- src/pages/Partners.tsx
+++ src/pages/Partners.tsx
@@ -4,39 +4,50 @@ import { Link } from 'react-router-dom';
 const Partners = () => {
   const partnerCategories = [
     {
-      title: 'Strategic Partners',
+      title: 'Platinum Sponsor',
       icon: <Building2 className="h-12 w-12 text-[#962326]" />,
+      partners: [
+        {
+          name: 'Momentum Health Solutions',
+          logo: '/images/Sponsors/Momentum.webp',
+          description: 'Leading research and development in healthcare solutions',
+          webLink: 'https://www.momentumhealthsolutions.co.za/'
+        }
+      ]
+    },
+    {
+      title: 'Our Partners',
+      icon: <Users className="h-12 w-12 text-[#962326]" />,
       partners: [
         {
           name: 'South African Medical Association',
           logo: '/images/Sponsors/SAMA.jpg',
-          description: 'Leading research and development in healthcare solutions'
+          description: 'The South African Medical Association (SAMA): A non-statutory, professional association for public- and private-sector medical practitioners in South Africa.'
         },
         {
-          name: 'S.A A.H.I.P V.H.I.A',
-          logo: '/images/Sponsors/7.png',
-          description: 'Advancing medical excellence across Africa'
+          name: 'Democratic Nursing Organisation of South Africa',
+          logo: '/images/Sponsors/Denosa-Logo.png',
+          description: 'A united and proficient nursing fraternity'
+        },
+        {
+          name: 'The Department of Public Service and Administration',
+          logo: '/images/Sponsors/DPSA-Logo.jpg',
+          description: 'Batho Pele: We Belong, We Care, We Serve.'
         },
         { 
           name: 'South African Medical Association Trade Union',
           logo: '/images/Sponsors/4.png',
           description: 'Leading research and development in healthcare solutions'
         },
-      ]
-    },
-    {
-      title: 'Healthcare Providers',
-      icon: <Users className="h-12 w-12 text-[#962326]" />,
-      partners: [
         {
           name: 'The Department of Health Mpumalanga',
           logo: '/images/Sponsors/mpumahealthdept.jpg',
           description: 'Improving health status through the prevention of illnesses and the promotion of healthy lifestyles and to consistently improve the healthcare delivery system by focusing on access, equity, efficiency, quality and sustainability.'
         },
         {
-          name: 'Denosa',
-          logo: '/images/Sponsors/Denosa-Logo.png',
-          description: 'The Democratic Nursing Organisation of South Africa'
+          name: 'The Department of Health Gauteng',
+          logo: '/images/Sponsors/Gauteng.png',
+          description: 'Our mission is to create an effective public healthcare system in Gauteng by ensuring we have the right people, skills, system and equipment to provide the care our patients need to live healthy and quality lives.'
         }
       ]
     },
@@ -45,9 +56,9 @@ const Partners = () => {
       icon: <Handshake className="h-12 w-12 text-[#962326]" />,
       partners: [
         {
-          name: 'Project Artour',
-          logo: '/images/Sponsors/11.png',
-          description: 'Fashion on the go'
+          name: 'Curro Academy Mbombela',
+          logo: '/images/Sponsors/curro.jpg',
+          description: 'Providing excellent education to learners from Grade R to Grade 7.'
         },
         {
           name: 'Inikiwe Consulting Services',
@@ -55,24 +66,40 @@ const Partners = () => {
           description: 'A finance and business consulting firm'
         },
         {
-          name: 'Stayeasy | Mbombela',
-          logo: '/images/Sponsors/3.png',
-          description: 'A travelling and accommodation agency'
+          name: 'Ndalo Group of Hotels',
+          logo: '/images/Sponsors/ndalo.jpg',
+          description: 'Nestled in four distinct locations, our hotels are a testament to luxury, comfort, and a commitment to providing unparalleled guest experiences.'
         },
         {
-          name: 'Jumpstart',
-          logo: '/images/Sponsors/1.png',
-          description: 'The organization that seeks to develop youth and women in rural areas to be prospective employees and entrepreneurs through skills development.'
+          name: 'Lancet Laboratories',
+          logo: '/images/Sponsors/lancet.png',
+          description: 'Lancet Laboratories SA is one of the leading pathology laboratories operating throughout South Africa, providing vital diagnostic and monitoring pathology services in 9 provinces. Cerba Lancet Africa provides services to Botswana, Gabon, Ghana, Kenya, Mozambique, Rwanda, Nigeria, Eswatini, Tanzania, Uganda, Zimbabwe and Zambia.'
         },
         {
-          name: 'Gozone Water',
-          logo: '/images/Sponsors/10.png',
-          description: 'We deliver only the healthiest remineralised ozonated water'
+          name: 'Dr Sifiso Nxumalo',
+          logo: '/images/Sponsors/drscnxumalo.png',
+          description: 'Dr. Sifiso C Nxumalo’s Medical Practice is where Advanced Healthcare meets Personalized Treatment, call for quality care in a welcoming environment. Our facilities, equipped with the latest technology, offer a broad spectrum of medical services. '
         }, 
         {
-          name: 'Old Mutual',
-          logo: '/images/Sponsors/12.png',
-          description: 'Investments, Insurance & More'
+          name: 'La Roche-Posay',
+          logo: '/images/Sponsors/laroche.jpg',
+          description: 'A BETTER LIFE FOR SENSITIVE SKIN'
+        },
+        {
+          name: 'The Nestlé Nutrition Institute (NNI)',
+          logo: '/images/Sponsors/NNIA.webp',
+          description: 'The Nestlé Nutrition Institute (NNI) is a non for profit association that shares leading science-based information.'
+        },
+        {
+          name: 'URIAGE',
+          logo: '/images/Sponsors/uriage.png',
+          description: 'Since 1992, the Dermatological Laboratories have incorporated the benefits of Uriage Thermal Water into its expert dermatological products to help all the family skin on a daily basis. Today, its expertise is recognised in over 70 countries.'
+        },
+        {
+          name: 'Clinical Care Platform',
+          logo: '/images/Sponsors/ccp.png',
+          description: 'Your Home for Continuous Professional Development (CPD). Welcome to Clinical Care Platform. Enjoy a Library of Over 100 CPD Courses.'
+          
         }
       ]
     }
