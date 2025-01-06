6e18f0c73b7949e7c36768cdf3b8c82ca2956ade
Immaculate Nyoni <sthwaloe@gmail.com>
2025-01-07 00:07:36 +0200

    done

--- src/pages/Winners.tsx
+++ src/pages/Winners.tsx
@@ -49,7 +49,6 @@ const Winners = () => {
 
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-12">
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-8">
-          { }
           {currentWinners.map((winner) => (
             <div
               key={winner.id}
